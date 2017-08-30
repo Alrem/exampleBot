e99c2649fdb6dd3fecf3d9003ee5c79d75be6fbe
Marina Lebedeva <Marycka9@yandex.ru>
2017-08-30 12:46:37 +0300

    Live bot

--- config.py
+++ config.py
@@ -1,9 +1,1 @@
-token = '395135141:AAG02WcogFYr4gFpSFlhtXfqSthiTqMeKWo'
 tokenLive = '426836109:AAH8RMiA5ZdvW0ReYvYkqPDO0b61UVA8pqQ'
-
-
-# Payment providers for Lumotest BOT @Lumotest_Bot.
-#
-# 2 methods connected:
-# - Paymentwall Test: 361519591:TEST:fd5ecf282f5637d92bba385fb639ccb7 2017-08-25 07:48
-# - Paymentwall Test: 361519591:TEST:86af836e28b29368f951e510928d9c60 2017-08-25 07:45
